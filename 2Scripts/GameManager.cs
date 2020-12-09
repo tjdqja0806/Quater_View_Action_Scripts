67a7b368ff7917bdcec13f5314fe6fd33f78f3ed
tjdqja0806 <74092254+tjdqja0806@users.noreply.github.com>
2020-12-09 17:14:52 +0900

    Add files via upload

---
+++ 2Scripts/GameManager.cs
@@ -0,0 +1,218 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject menuCam;
+    public GameObject gameCam;
+    public Player player;
+    public Boss boss;
+    public GameObject itemShop;
+    public GameObject weaponShop;
+    public GameObject startZone;
+    public int stage;
+    public float playTime;
+    public bool isBattle;
+    public int enemyCntA;
+    public int enemyCntB;
+    public int enemyCntC;
+    public int enemyCntD;
+
+    public Transform[] enemyZones;
+    public GameObject[] enemies;
+    public List<int> enemyList;
+
+    public GameObject menuPanel;
+    public GameObject gamePanel;
+    public GameObject OverPanel;
+    public Text maxScoreText;
+    public Text scoreText;
+    public Text stageText;
+    public Text playTimeText;
+    public Text playerHealthText;
+    public Text playerAmmoText;
+    public Text playerCoinText;
+    public Image weapon1Image;
+    public Image weapon2Image;
+    public Image weapon3Image;
+    public Image weaponRImage;
+    public Text enemyAText;
+    public Text enemyBText;
+    public Text enemyCText;
+    public RectTransform bossHealthGroup;
+    public RectTransform bossHealthBar;
+    public Text curScoreText;
+    public Text bestText;
+
+    void Awake()
+    {
+        enemyList = new List<int>();
+        maxScoreText.text = string.Format("{0:n0}", PlayerPrefs.GetInt("MaxScore"));
+
+        if (PlayerPrefs.HasKey("MaxScore"))
+            PlayerPrefs.SetInt("MaxScore", 0);
+    }
+
+    public void GameStart()
+    {
+        menuCam.SetActive(false);
+        gameCam.SetActive(true);
+
+        menuPanel.SetActive(false);
+        gamePanel.SetActive(true);
+
+        player.gameObject.SetActive(true);
+    }
+    public void GameOver()
+    {
+        gamePanel.SetActive(false);
+        OverPanel.SetActive(true);
+        curScoreText.text = scoreText.text;
+
+        int maxScore = PlayerPrefs.GetInt("MaxScore");
+        if(player.score > maxScore)
+        {
+            bestText.gameObject.SetActive(true);
+            PlayerPrefs.SetInt("MaxScore", player.score);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void StageStart()
+    {
+        itemShop.SetActive(false);
+        weaponShop.SetActive(false);
+        startZone.SetActive(false);
+
+        foreach (Transform zone in enemyZones)
+            zone.gameObject.SetActive(true);
+
+        isBattle = true;
+        StartCoroutine(InBattle());
+    }
+    public void StageEnd()
+    {
+        player.transform.position = Vector3.up * 0.8f;
+
+        itemShop.SetActive(true);
+        weaponShop.SetActive(true);
+        startZone.SetActive(true);
+
+        foreach (Transform zone in enemyZones)
+            zone.gameObject.SetActive(false);
+
+        isBattle = false;
+        stage++;
+    }
+
+    IEnumerator InBattle()
+    {
+        if(stage % 5 == 0)
+        {
+            enemyCntD++;
+            GameObject instantEnemy = Instantiate(enemies[3], enemyZones[0].position, enemyZones[0].rotation);
+            Enemy enemy = instantEnemy.GetComponent<Enemy>();
+            enemy.target = player.transform;
+            enemy.maneger = this;
+            boss = instantEnemy.GetComponent<Boss>();
+        }
+        else
+        {
+            for (int index = 0; index < stage; index++)
+            {
+                int ran = Random.Range(0, 3);
+                enemyList.Add(ran);
+
+                switch (ran)
+                {
+                    case 0:
+                        enemyCntA++;
+                        break;
+                    case 1:
+                        enemyCntB++;
+                        break;
+                    case 2:
+                        enemyCntC++;
+                        break;
+                }
+            }
+
+            while (enemyList.Count > 0)
+            {
+                int ranZone = Random.Range(0, 4);
+                GameObject instantEnemy = Instantiate(enemies[enemyList[0]], enemyZones[ranZone].position, enemyZones[ranZone].rotation);
+                Enemy enemy = instantEnemy.GetComponent<Enemy>();
+                enemy.target = player.transform;
+                enemy.maneger = this;
+                enemyList.RemoveAt(0);
+                yield return new WaitForSeconds(4f);
+            }
+        }
+
+        while (enemyCntA + enemyCntB + enemyCntC + enemyCntD > 0)
+        {
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(4f);
+
+        boss = null;
+        StageEnd();
+    }
+
+    void Update()
+    {
+        if (isBattle)
+            playTime += Time.deltaTime;
+    }
+
+    void LateUpdate()
+    {
+        //상단 UI
+        scoreText.text = string.Format("{0:n0}", player.score);
+        stageText.text = "STAGE " + stage;
+
+        int hour = (int)(playTime / 3600);
+        int min = (int)((playTime - hour * 3600) / 60);
+        int second = (int)(playTime % 60);
+        playTimeText.text = string.Format("{0:00}", hour) + ":" + string.Format("{0:00}", min) + ":" + string.Format("{0:00}", second);
+
+        //플레이어 UI
+        playerHealthText.text = player.health + " / " + player.maxHealth;
+        playerCoinText.text = string.Format("{0:n0}", player.coin);
+        if (player.equipWeapon == null)
+            playerAmmoText.text = "- / " + player.ammo;
+        else if (player.equipWeapon.type == Weapon.Type.Melee)
+            playerAmmoText.text = "- / " + player.ammo;
+        else
+            playerAmmoText.text = player.equipWeapon.curAmmo + " / " + player.ammo;
+
+        //무기 UI
+        weapon1Image.color = new Color(1, 1, 1, player.hasWeapons[0] ? 1 : 0);
+        weapon2Image.color = new Color(1, 1, 1, player.hasWeapons[1] ? 1 : 0);
+        weapon3Image.color = new Color(1, 1, 1, player.hasWeapons[2] ? 1 : 0);
+        weaponRImage.color = new Color(1, 1, 1, player.hasGrenades > 0 ? 1 : 0);
+
+        //몬스터 숫자 UI
+        enemyAText.text = enemyCntA.ToString();
+        enemyBText.text = enemyCntB.ToString();
+        enemyCText.text = enemyCntC.ToString();
+
+        //보스 체력 UI
+        if(boss != null)
+        {
+            bossHealthGroup.anchoredPosition = Vector3.down * 30;
+            bossHealthBar.localScale = new Vector3((float)boss.curHealth / boss.maxHealth, 1, 1);
+        }
+        else
+        {
+            bossHealthGroup.anchoredPosition = Vector3.up * 200;
+        }
+    }
+}
---
+++ 2Scripts/Weapon.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public enum Type { Melee, Range };
+    public Type type;
+    public int damage;
+    public float rate;
+    public int maxAmmo;
+    public int curAmmo;
+
+    public BoxCollider meleeArea;
+    public TrailRenderer trailEffect;
+    public Transform bulletPos;
+    public GameObject bullet;
+    public Transform bulletCasePos;
+    public GameObject bulletCase;
+
+    public void Use()
+    {
+        if(type == Type.Melee)
+        {
+            StopCoroutine("Swing"); //코루틴 정지 함수
+            StartCoroutine("Swing");//코루틴 시작 함수
+        }
+        else if (type == Type.Range && curAmmo > 0)
+        {
+            curAmmo--;
+            StartCoroutine("Shot");//코루틴 시작 함수
+        }
+    }
+     IEnumerator/* 열서형 함수 클래스*/ Swing()
+    {
+        yield return new WaitForSeconds(0.3f); //0.3초 대기
+        meleeArea.enabled = true;
+        trailEffect.enabled = true;
+
+        yield return new WaitForSeconds(0.3f);
+        meleeArea.enabled = false;
+
+        yield return new WaitForSeconds(0.3f);
+        trailEffect.enabled = false;
+    }
+
+    //Use() 메인루틴 -> Swing() 서브루틴 -> Use() 메인루틴(순서대로 실행)
+    //Use() 메인루틴 + Swing() 코루틴(함께 실행)
+    // yield return null;  = 1프레임 대기
+    //yield return new WaitForSeconds(0.nf); //0.n초 대기
+
+    IEnumerator Shot()
+    {
+        //#1.총알 발사
+        GameObject instantBullet = Instantiate(bullet, bulletPos.position, bulletPos.rotation);//총알 생성
+        Rigidbody bulletRigid = instantBullet.GetComponent<Rigidbody>();
+        bulletRigid.velocity = bulletPos.forward * 100;//총알 속도
+
+        yield return null;
+        //#2.탄피 배출
+        GameObject instantCase = Instantiate(bulletCase, bulletCasePos.position, bulletCasePos.rotation);
+        Rigidbody caseRigid = instantCase.GetComponent<Rigidbody>();
+        Vector3 caseVec = bulletCasePos.forward * Random.Range(-3, -2) + Vector3.up * Random.Range(2, 3);
+        caseRigid.AddForce(caseVec, ForceMode.Impulse);
+        caseRigid.AddTorque(Vector3.up * 10, ForceMode.Impulse);
+    }
+}
